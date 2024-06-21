35eb5d6ee928adf16a0f8991d60d0e154bcf836b
babuyaga <77667412+babuyaga@users.noreply.github.com>
2024-06-21 01:10:49 -0400

    Implement navbar scroll color change

--- src/components/navbar/TopNavbar.jsx
+++ src/components/navbar/TopNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect ,useState} from "react";
 import NavbarItem from "./NavbarItem.jsx";
 import SearchInput from "./SearchInput.jsx";
 import MenuButtonHolder from "./MenuButtonHolder.jsx";
@@ -7,11 +7,29 @@ import "./topnavbar.scss";
 
 
 function TopNavbar(prop){
+  const [header, setHeader] = useState({color:"#000000",borderLeft: "5px solid #000000"})
 
+const listenScrollEvent = (event) => {
+  if (window.scrollY < 1440) {
+    console.log("scrolled");
+    return setHeader({color:"#000000",    borderLeft: "5px solid #000000"})
+  } else if (window.scrollY > 1440) {
+    console.log("scrolled back");
+    return setHeader({color:"#ffffff",    borderLeft: "5px solid #ffffff"})
+  
+  } 
+}
+
+useEffect(() => {
+  window.addEventListener('scroll', listenScrollEvent);
+
+  return () =>
+    window.removeEventListener('scroll', listenScrollEvent);
+}, []);
 
 return(<div className="topnavbar" style={{backgroundColor:"ffffff"}}>
           <div className="topnavbar__width-control" >
-            <NavbarItem id="logo"> <div className="comp__logo"><h1>Jerry Jose</h1></div> </NavbarItem>
+            <NavbarItem id="logo"> <div className="comp__logo"><h1 style={header}>Jerry Jose</h1></div> </NavbarItem>
         {/* <NavbarItem id="search"> <SearchInput placeholdercomp="Search SilverVault" onFormSubmit={e=>e.preventDefault()}/> </NavbarItem> */}
                 {/* <NavbarItem id="menu"><MenuButtonHolder screen="menu__desktop" icons={iconsdesk}/></NavbarItem> */}
                 <MenuButtonHolder screen="menu__phone" icons={iconsphone}/>
